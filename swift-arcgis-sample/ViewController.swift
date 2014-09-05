a94914630401a757f0e0b8ad76eed5d9c4b7c04d
Eric Ito <eric.ito@gmail.com>
2014-09-05 14:06:34 -0700

    add files

---
+++ swift-arcgis-sample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  swift-arcgis-sample
+//
+//  Created by Eric Ito on 9/5/14.
+//  Copyright (c) 2014 Esri. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
