f809ee6546cd71eeeb0aa1166a126d85e5ec19a7
Eric Ito <eric.ito@gmail.com>
2014-09-05 14:26:04 -0700

    add code

--- swift-arcgis-sample/ViewController.swift
+++ swift-arcgis-sample/ViewController.swift
@@ -7,12 +7,18 @@
 //
 
 import UIKit
+import ArcGIS
 
 class ViewController: UIViewController {
                             
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        var mapView = AGSMapView(frame: view.bounds)
+        mapView.autoresizingMask = .FlexibleWidth | .FlexibleHeight
+        mapView.addMapLayer(AGSOpenStreetMapLayer())
+        view.addSubview(mapView)
     }
 
     override func didReceiveMemoryWarning() {
